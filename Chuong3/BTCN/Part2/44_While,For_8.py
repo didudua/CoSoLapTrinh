b84950caad7708ade27db18c1da12ebe9ecc4889
didudua <126039750+didudua@users.noreply.github.com>
2023-03-09 10:49:17 +0700

    ea

--- Chuong3/BTCN/Part2/44_While,For_8.py
+++ Chuong3/BTCN/Part2/44_While,For_8.py
@@ -1,6 +1,4 @@
 a = input()
 n = int(input())
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        print(a, end=" ")
-    print("")
+for i in range(1,n+1):
+    print((str(a)+' ')*i)
--- Chuong5/BTCN/vd.py
+++ Chuong5/BTCN/vd.py
@@ -1,1 +1,8 @@
-print('helo')
+# a='ell'
+# for i in range(len(a)):
+#         print(i)
+#         print(a[i])
+
+for i in range(1,7):
+    print('* '*i)
+
