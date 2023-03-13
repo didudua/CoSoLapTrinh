a45108c723427e9482663fe2f31eef311675ae7a
didudua <126039750+didudua@users.noreply.github.com>
2023-03-13 21:45:50 +0700

    mới

--- Chuong5/BT_Slide/44_BTOT.py
+++ Chuong5/BT_Slide/44_BTOT.py
@@ -1,48 +1,56 @@
-L=[1,2,3,4,5,5,7]
+L = [1, 2, 3, 4, 5, 6, 7]
+
 def nhap():
-    x=input('nhap x: ')
-    k=int(input('vi tri k: '))
-    y=input('y thay the x: ')
-    return x,k,y
-def add(L,x,k):
+    x = input('nhap x: ')
+    k = int(input('vi tri k: '))
+    y = input('y thay the x: ')
+    return x, k, y
+
+def add(L, x, k):
     if k > len(L):
-        L=L.append(x) # them x vao vi tri cuoi cua day
-    else: L=L.insert(k,x) # them x vao vi tri k cua day
-    return L
-#cau 2
-def search(L,x):
-    if x in L:
-        u=L.index(x)
-        return u
-    else:  return None
-#cau 3
-def delete(L,x):
-    if x in L:
-        L=L.clear(x)
-    return L
-#cau4
-def count(L) :
-    c=len(L)
-    return c
-#cau5
-def update(L,x,y):
-    if x in L:
-        L=L.insret(L.index(x),y)
+        L.append(x) # them x vao vi tri cuoi cua day
+    else:
+        L.insert(k, x) # them x vao vi tri k cua day
     return L
 
-x,k,y=nhap()
+#cau 2
+def search(L, x):
+    if x in L:
+        u = L.index(x)
+        return u
+    else:
+        return None
+
+#cau 3
+def delete(L, x):
+    if x in L:
+        L.remove(x)
+    return L
+
+#cau4
+def count(L) :
+    return len(L)
+
+#cau5
+def update(L, x, y):
+    if x in L:
+        L.insert(L.index(x), y)
+        L.remove(x)
+    return L
+
+x, k, y = nhap()
 print('cau1')
-add(L,x,y)
+L = add(L, x, k)
 print(L)
 print('cau2')
-u=search(L,x)
+u = search(L, x)
 print(u)
 print('cau3')
-delete(L,x)
+L = delete(L, x)
 print(L)
 print('cau4')
-c=count(L)
+c = count(L)
 print(c)
 print('cau5')
-update(L,x,y)
+L = update(L, x, y)
 print(L)
