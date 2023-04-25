721b1cb11e9b7bae58e8111e6e350a44f240688a
didudua <126039750+didudua@users.noreply.github.com>
2023-04-25 15:51:37 +0700

    upa

--- Chuong7/BTCN/44_2.py
+++ Chuong7/BTCN/44_2.py
@@ -1,13 +1,14 @@
 dau=',.;:'
 L=input().strip(' ')
 L=L.capitalize()
-for j in range(2,len(L)):
-    L= ''.join(L.split(j*' '))
 L=list(L)
 i=0
 while i<len(L):
     if L[i] in dau:
-        if L[i-1]==' ':
-            L.pop(i-1)
-    i+=1
+        for j in range(i-1,-1,-1):
+            if L[j]==' ':
+                L.pop(j)
+            else: break
+    else:
+        i+=1
 print(''.join(L))
